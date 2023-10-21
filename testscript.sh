5c16bceb78e4dfd9583620b684c9180c3e8aa412
DiospyrosD <51894165+DiospyrosD@users.noreply.github.com>
2023-10-21 18:21:15 -0400

    Create testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the local IP address
+HOST_IP=$(ip a | grep -o 'inet 10\.[0-9]\+\.[0-9]\+\.[0-9]\+' | awk '{print $2}' | head -n 1)
+
+# Run the SSH command to retrieve data from alpha
+SSH_OUTPUT=$(ssh student@alpha "curl $HOST_IP:5555")
+
+# Echo the output to everyone ssh'd into the machine
+echo "Output from alpha: $SSH_OUTPUT" | wall
+echo "Remember, I'm SSH'ing into alpha and targetting bchd host (IP: $HOST_IP)" | wall
