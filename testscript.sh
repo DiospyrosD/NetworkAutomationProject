87808dd5c89efc8674b97c8822efc1cc8652023f
DiospyrosD <51894165+DiospyrosD@users.noreply.github.com>
2023-10-21 19:38:05 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -4,7 +4,7 @@
 HOST_IP=$(ip a | grep -o 'inet 10\.[0-9]\+\.[0-9]\+\.[0-9]\+' | awk '{print $2}' | head -n 1)
 
 # Run the SSH command to retrieve data from alpha
-SSH_OUTPUT=$(ssh student@alpha "curl $HOST_IP:5555")
+SSH_OUTPUT=$(sudo -u student ssh student@alpha "curl $HOST_IP:5555")
 
 # Echo the output to everyone ssh'd into the machine
 echo "Output from alpha: $SSH_OUTPUT" | wall
