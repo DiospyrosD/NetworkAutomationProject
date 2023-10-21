ccc31db1c51e0c4df7613c248bb88a253e5efd4f
DiospyrosD <51894165+DiospyrosD@users.noreply.github.com>
2023-10-21 15:53:08 -0400

    Create web_server.py

---
+++ web_server.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+   return "This is the xxxx subnet\n"
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=9999)
