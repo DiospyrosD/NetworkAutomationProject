33ae30c32993700c85a09a199e32c3eeb7c10b8b
DiospyrosD <51894165+DiospyrosD@users.noreply.github.com>
2023-10-23 00:05:57 -0400

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "This is the xxxx subnet\n"
+   return "This is the whiskey subnet\n"
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=9999)
