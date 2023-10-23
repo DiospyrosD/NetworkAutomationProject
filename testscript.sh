7d979eb1eafd862fbc4b4f869e9fc53342b490a8
DiospyrosD <51894165+DiospyrosD@users.noreply.github.com>
2023-10-23 09:20:42 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,7 +3,7 @@
 # Get the local IP address
 HOST_IP=$(ip a | grep -o 'inet 10\.[0-9]\+\.[0-9]\+\.[0-9]\+' | awk '{print $2}' | head -n 1)
 
-# Run the SSH command to retrieve data from alpha
+# Run the SSH command to retrieve data from alpha. Remember we are targeting our local machine BCHD on port 5555.
 SSH_OUTPUT=$(sudo -u student ssh student@alpha "curl $HOST_IP:5555")
 
 # Echo the output to everyone ssh'd into the machine
